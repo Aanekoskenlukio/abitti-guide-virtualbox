52a1ccc16d7d66007c8b5af8ec1b3463240fd561
xrt.li <in@xrt.li>
2018-04-28 21:19:01 +0300

    add comments

--- script.ps1
+++ script.ps1
@@ -1,29 +1,44 @@
 # xrtli@GitHub
 # Käynnistää virtuaalisen Abitti-palvelimen automatisoidusti
+# Initiates a virtual Abitti server instance automatically
 
+# Set environmental variables
 $env:HOMEDRIVE="C:"
 $env:HOMEPATH="\Users\" + $Env:USERNAME + "\"
 
+# Declare a full path to the ktp directory
 $FULLPATH = $env:HOMEDRIVE + $env:HOMEPATH + "ktp"
+cd $FULLPATH
+
+# Being in the ktp dir,
+# set the ktp-jako dir relative path
+$EXAMPATH = "..\ktp-jako"
+
+# Print out the full ktp path
 echo ""
 echo "Poluksi valittu:"
 echo $FULLPATH
 echo ""
 echo ""
 
-cd $FULLPATH
-
+# Print out the exam dir path
 echo ""
 echo ""
 echo "Etsitään kokeita hakemistosta:"
-echo "ktp-jako"
+echo $EXAMPATH
 echo ""
 echo ""
 
-echo "------- [ PALVELIMELLA OLEVAT KOKEET ] -------\n\n"
+echo "------- [ PALVELIMELLA OLEVAT KOKEET ] -------"
 
-Get-ChildItem -Path ..\ktp-jako
+# List all the files inside of ktp-jako exam folder
+Get-ChildItem -Path $EXAMPATH
 echo ""
 echo "Varmista, että koe on listattu yllä. Jatketaan automaattisesti..."
-sleep(6)
+
+# Sleep for 2s to ensure the teacher sees
+# the exam listing printed above
+sleep(2)
+
+# Initiate a vagrant session
 vagrant up
